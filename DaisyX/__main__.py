da761b70d5f574b1d406d2a7938095fd1e6a46b0
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-06 12:30:22 +0700

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -369,7 +369,7 @@ def DaisyX_about_callback(update, context):
             f"\n✪ Saya memiliki sistem pencatatan, daftar hitam, dan bahkan balasan yang telah ditentukan sebelumnya pada kata kunci tertentu.\n"
             f"\n✪ Saya memeriksa izin admin sebelum menjalankan perintah apa pun dan lebih banyak barang\n"
             f"\n\n𝗣𝗥𝗔𝗕𝗨 licensed under the GNU General Public License v3.0\n"
-            f"\n✪ Pengembang saya: [sᴛᴇᴠᴀɴ](https://t.me/GZ_056) \n"
+            f"\n✪ Pengembang saya: [sᴛᴇᴠᴀɴ](https://t.me/GZ_056), \n"
             f"\n\nJika Anda memiliki pertanyaan tentang 𝗣𝗥𝗔𝗕𝗨, beri tahu kami\n",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
