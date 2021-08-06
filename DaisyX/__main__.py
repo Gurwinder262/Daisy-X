427d1a0a8cfbc02d362eee64814ac1113c2fd700
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-06 14:33:27 +0700

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -749,7 +749,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online!")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "𝗣𝗥𝗔𝗕𝗨 Sudah aktif!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
