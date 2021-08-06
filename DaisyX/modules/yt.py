66241eb84b15454e6d8444ae7fd28b3ba05f40c6
Stevan KZ <87872818+StevanKz@users.noreply.github.com>
2021-08-06 21:42:00 +0700

    Update yt.py

--- DaisyX/modules/yt.py
+++ DaisyX/modules/yt.py
@@ -1,5 +1,5 @@
 __help__ = """
- - /yt <text>: perform a youtube search
- - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+ - /yt <text>: melakukan pencarian youtube
+ - /ytaudio <link> atau /yt video <link>: Mengunduh video atau audio dari video youtube ke server lokal bot
 """
 __mod_name__ = "Youtube 🎞"
